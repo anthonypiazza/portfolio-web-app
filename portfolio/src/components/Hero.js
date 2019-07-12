edfba5d53acec7b2c3a90b6e278533a4e508508f
anthonypiazza <piazza.anthony.n@gmail.com>
2019-07-12 13:41:57 -0500

    Added sticky nav, changed nav to section scroll SPA

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -3,30 +3,43 @@ import {NavLink} from 'react-router-dom';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import '../components/css/hero.css';
+import { Link, animateScroll as scroll } from "react-scroll";
 import ProfileImg from '../../src/imgs/Anthony_Piazza.jpg';
 
-const Hero = () => {
+const Hero = props => {
     return(
-        <section className="hero-content">
+        <section className="hero-content" id={props.id}>
             <AwesomeSlider className="slider-div">
                 <div className="hero-width-div">
-                    <div className="hero-text slide-in-left">
-                        <div>
+                    <div className="hero-text">
+                        <div className="job-occupation">
                             <h1>Full Stack Web Developer</h1>    
                         </div>
                         <div className="tagline">
-                            <p>Currently enrolled in the Lambda School Full Stack Web Development Program.</p>
+                            <p>Enrolled in the Lambda School Full Stack Web Development Program.</p>
                         </div>
                     </div>
                     <div className="button-div">
-                        <NavLink className="learn-more" link to="https://boring-euclid-601ad3.netlify.com/">Learn More</NavLink>
+                        <Link spy={true} smooth={true} offset={-70} duration= {500} className="learn-more" to="section2">Learn More</Link>
                     </div>
                 </div>
-              <div className="second-container">
-                    <div className="hero-image">
-                        <img src={ProfileImg} alt="Anthony Piazza"/>
+                <div className="second-container">
+                    <div className="hero-text">
+                        <div>
+                            <h1 className="second-header">San Diego State University Class of 2016</h1>    
+                        </div>
+                        <div className="second-tagline">
+                            <p className="degree">Bachelor of Science in Business Administration</p>
+                            <p className="education-details">Dean's List - Spring of 2014 and Spring of 2015</p>
+                            <p className="education-details">Studied at Oxford University during Summer 2015 session (Pacific University Exchange Program)</p>
+                            <p className="education-details">Sigma Chi Fraternity & Associated Student Leadership</p>
+
+                        </div>
                     </div>
-              </div>
+                    {/* <div className="hero-image">
+                        <img src={ProfileImg} alt="Anthony Piazza"/>
+                    </div> */}
+                </div>
               <div><img src="/path/to/image-0.png"/></div>
             </AwesomeSlider>
         </section>   
--- portfolio/src/components/Mission.js
+++ portfolio/src/components/Mission.js
@@ -8,11 +8,30 @@ const Mission = () => {
                 <h2>My Developer Skillset and Toolstack Includes...</h2>
             </div>
             <div className="boxes">
-                <div className="box-div"><i class="fab fa-react"></i></div>
-                <div className="box-div"><i class="fab fa-js"></i></div>
-                <div className="box-div"><i class="fab fa-css3-alt"></i></div>
-                <div className="box-div"><i class="fab fa-html5"></i></div>
-                <div className="box-div"><i class="fab fa-less"></i></div>
+                <div className="box-div">
+                    <i class="fab fa-react"></i>
+                    <p>ReactJS</p>
+                </div>
+                <div className="box-div">
+                    <i class="fab fa-js"></i>
+                    <p>Javascript</p>
+                </div>
+                <div className="box-div">
+                    <i class="fab fa-css3-alt"></i>
+                    <p>CSS3</p>
+                </div>
+                <div className="box-div">
+                    <i class="fab fa-html5"></i>
+                    <p>HTML5</p>
+                </div>
+                <div className="box-div">
+                    <i class="fab fa-less"></i>
+                    <p>LESS</p>
+                </div>
+                <div className="box-div">
+                    <i class="fab fa-git"></i>
+                    <p>GitFlow</p>
+                </div>
             </div>
         </section>        
     )
